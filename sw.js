8abda0c56299b157e6d7ee1ea6844996e9ed8b6b
santiagoacostav-stack <santiagoacostav@gmail.com>
2026-08-17 13:44:19 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='san-v1-8-1-calendar-fix';
-const A=['./','./index.html','./plan.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
